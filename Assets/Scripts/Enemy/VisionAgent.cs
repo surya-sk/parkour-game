6f1a59d55637db282f1e4172b2688574bbfbf940
surya-sk <surya.sk05@outlook.com>
2022-10-02 15:33:22 +0530

    Create functions to check range and angle

---
+++ Assets/Scripts/Enemy/VisionAgent.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParkourGame.Enemy
+{
+    public class VisionAgent : MonoBehaviour
+    {
+        public float VisionRange = 5f;
+        public float VisionAngle = 30f;
+        public Transform Target;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(!IsInRange()) { return; }
+
+        }
+
+        /// <summary>
+        /// Returns true if the player is in range
+        /// </summary>
+        /// <returns></returns>
+        private bool IsInRange()
+        {
+            var _distance = Vector3.Distance(transform.position, Target.position);
+            return _distance < VisionRange;
+        }
+
+        /// <summary>
+        /// Returns true if the player falls under the vision angle
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckAngle()
+        {
+            var _targetDirection = Target.position - transform.position;
+            float _angle = Vector3.Angle(_targetDirection, transform.forward);
+            return _angle < VisionAngle;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(transform.position, VisionRange);
+
+            Quaternion _rot = Quaternion.AngleAxis(VisionAngle, Vector3.up);
+            Vector3 _endPoint = transform.position + (_rot * transform.forward * VisionRange);
+            Gizmos.DrawLine(transform.position, _endPoint);
+
+            _rot = Quaternion.AngleAxis(-VisionAngle, Vector3.up);
+            _endPoint = transform.position + (_rot * transform.forward * VisionRange);
+            Gizmos.DrawLine(transform.position, _endPoint);
+        }
+
+    }
+}
